81203ff177754a4e0be3ec40d465f462c70d19b7
redga <redga@DESKTOP-IJEVMHT>
2022-05-04 23:57:09 -0400

    Programa version final(2), agregamos la clase reporte

---
+++ src/main/java/Reporte.java
@@ -0,0 +1,38 @@
+/*import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;*/
+
+public class Reporte{
+/*  public static Persona persona = new Persona();
+  public static Fecha fecha = new Fecha();
+  public static Producto producto = new Producto();
+
+  //Leemos la ruta del archivo csv.
+  String path = "/Users/redga/Desktop/baseDAtos.CSV";
+            String line = "";
+  try{
+                BufferedReader br = new BufferedReader(new FileReader(path));
+
+                while((line = br.readLine()) !=null){
+                    String[] values = line.split(",");
+                    
+                    //Lee y almacena los datos de las variables.
+                    persona.setNombre(values[0]);
+                    fecha.setFecha(values[1]);
+                    producto.setNombre(values[2]);
+
+                    //System.out.println(persona.getNombre()+"  " + fecha.getFecha() + " " + producto.getNombre());
+                }
+            }
+            //En caso de error.
+            catch (FileNotFoundException e){
+                e.printStackTrace();
+            } 
+            catch (IOException e){
+                e.printStackTrace();  
+            }
+*/
+}
+
