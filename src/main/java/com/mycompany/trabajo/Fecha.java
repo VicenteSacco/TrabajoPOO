2e2b6a807a9009472a259bd06452719232549477
redga <redga@DESKTOP-IJEVMHT>
2022-07-03 23:50:47 -0400

    Entrega final Del Proyecto

--- src/main/java/com/mycompany/trabajo/Fecha.java
+++ src/main/java/com/mycompany/trabajo/Fecha.java
@@ -1,63 +1,26 @@
 package com.mycompany.trabajo;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Fecha {
-    //Atributos
-    private String fecha;
     public ArrayList<ProductoSano> ListaProductoSano = new ArrayList<ProductoSano>();
     public ArrayList<ProductoNoSano> ListaProducto = new ArrayList<ProductoNoSano>();
+    
+    private Date fecha;
 
     public Fecha() {
     }
     
-    public Fecha(String fecha) {
+    public Fecha(Date fecha) {
         this.fecha = fecha;
     }
 
-    public String getFecha() {
+    public Date getFecha() {
         return fecha;
     }
 
-    public void setFecha(String fecha) {
+    public void setFecha(Date fecha) {
         this.fecha = fecha;
-    }    
-    
-    public void ImprimirProducto(){
-        for(int i = 0 ; i < ListaProducto.size() ; i++){
-            System.out.println("=====ListaProductos=====");
-            System.out.println("Nombre: "+ListaProducto.get(i).getNombre());
-            System.out.println("Calorias: "+ListaProducto.get(i).getCalorias());
-            System.out.println("Cantidad: "+ListaProducto.get(i).getCantidad());
-        }
     }
-    
-    public void EliminarProducto(String nombre){
-        for(int i=0; i<ListaProducto.size(); i++){
-            if(nombre.equals(ListaProducto.get(i).getNombre())){
-                ListaProducto.remove(i);
-                break;
-            }
-        }
-    }
-    
-    public void ModificarProducto(String nombreActual, String nombreNuevo, int calorias, int cantidad, int sellos){
-        for(int i = 0 ; i < ListaProducto.size(); i++){
-            if(nombreActual.equals(ListaProducto.get(i).getNombre())){
-                ListaProducto.get(i).setNombre(nombreNuevo);
-                ListaProducto.get(i).setCalorias(calorias);
-                ListaProducto.get(i).setCantidad(cantidad);
-                ListaProducto.get(i).setSellos(sellos);
-            }
-        }
-    }
-  
-    public ProductoNoSano EncontrarProducto(String nombre){
-        for(int i = 0 ;i<ListaProducto.size();i++){
-            if(nombre.equals(ListaProducto.get(i).getNombre()))
-                return ListaProducto.get(i);
-        }
-        return null;
-    }
-    
-}
+}   
--- src/main/java/com/mycompany/trabajo/Principal.java
+++ src/main/java/com/mycompany/trabajo/Principal.java
@@ -1,24 +1,17 @@
 package com.mycompany.trabajo;
 
-/*
-  Vicente Sacco 20.502.134-5
-  
-  Mauro Alicera 19.980.080-9
+import java.util.ArrayList;
 
-  Jorge Gonzalez 18.236.888-1
-*/
-
-/**
- *
- * @author mauro
- */
 public class Principal {
     
-    
+    public static ArrayList<Fecha> ListaFecha ;
     
     public static void main(String[] args) {
-        Ventana ventana = new Ventana();
-        ventana.setVisible(true);
-        
+       
+        ListaFecha = new ArrayList<>();
+       
+        Registro r = new Registro();
+       
+        r.setVisible(true);
     }
 }
